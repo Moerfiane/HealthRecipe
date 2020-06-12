8c7296d1d90514c5fc3bfefb6c91b83b3f62615c
Kathy <kvsippo@charter.net>
2020-06-11 19:01:00 -0500

    selection page displaying all 4 columns dynamically and saving to database

--- queries.sql
+++ queries.sql
@@ -1,12 +1,43 @@
 ## Part 1: Test it with SQL
-SELECT* FROM health_recipe.job;
+
 
 --## Part 2: Test it with SQL
-SELECT name from employer
-WHERE location = 'St. Louis, MO';
 
 --## Part 3: Test it with SQL
-DROP TABLE job;
+
 
 --## Part 4: Test it with SQL
-SELECT name, description FROM health_recipe.skill WHERE id in(SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL);
+
+
+INSERT INTO dietary_restrictions_search (restrict_id, restrictions)
+    VALUES   (301,"No Carbohydrates"),
+            (302,"No Dairy"),
+            (303,"No Saturated Fats"),
+            (304,"No Sodium"),
+            (305,"Gluten Free"),
+            (306,"Nitrate Free"),
+            (307,"Sodium Free");
+
+INSERT INTO dietary_restrictions_search (restrict_id, health_conds)
+  VALUES   (101,"Diabetes"),
+		   (102,"High Blood Pressure"),
+		   (103,"Celiac Disease");
+
+INSERT INTO dietary_restrictions_search (restrict_id, ingred_search)
+    VALUES   (201,"Pork"),
+            (202,"Chicken"),
+            (203,"Beef"),
+            (204,"Cucumbers"),
+            (205,"Cheese"),
+            (206,"Peppers"),
+            (207,"Fish"),
+            (208,"Onions"),
+            (209,"Nuts"),
+            (210,"Vegetables");
+
+   INSERT INTO dietary_restrictions_search (restrict_id, cook_time)
+    VALUES   (401,"15 Minutes"),
+            (402,"30 Minutes"),
+            (403,"45 Minutes"),
+            (404,"60 Minutes");
+
