261f934c74e27dc85440489649d1e2f8a5cddccf
Michael Banvard <mjbanvard@gmail.com>
2020-07-17 16:09:17 -0500

    Added checks in CreateMySQLDatabase to cover ClearDB.

--- src/main/java/org/launchcode/health_recipe/CreateMySQLDatabase.java
+++ src/main/java/org/launchcode/health_recipe/CreateMySQLDatabase.java
@@ -9,13 +9,20 @@ import java.sql.PreparedStatement;
 public class CreateMySQLDatabase {
     public static void main(String[] args) {
         // Defines the JDBC URL.
-        // This is a JawsDB hosted on AmazonAWS, care of Michael Banvard. He's willing to keep
-        // it up for a while, to keep things moving forward.
-        String url = "jdbc:mysql://spyd1o2a1ywz4gbx:kdikqyzoq9kbmlro@qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zjn0rnwkv03hnqw6?serverTimezone=UTC";
+//        // This is a JawsDB hosted on AmazonAWS, care of Michael Banvard. He's willing to keep
+//        // it up for a while, to keep things moving forward.
+//        String url = "jdbc:mysql://spyd1o2a1ywz4gbx:kdikqyzoq9kbmlro@qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zjn0rnwkv03hnqw6?serverTimezone=UTC";
+//
+//        // Defines username and password to connect to database server.
+//        String username = "spyd1o2a1ywz4gbx";
+//        String password = "kdikqyzoq9kbmlro";
+
+        // This is a ClearDB hosted on AmazonAWS, care of Michael Banvard.
+        String url = "jdbc:mysql://jdbc:mysql://www.cleardb.com/database/details?id=0A30CBF9C827CB84EC09A18B40740747?serverTimezone=UTC";
 
         // Defines username and password to connect to database server.
-        String username = "spyd1o2a1ywz4gbx";
-        String password = "kdikqyzoq9kbmlro";
+        String username = "b7f364d4f033b1";
+        String password = "e5f46ebe";
 
         // SQL command to create a database in MySQL.
         String sql = "CREATE DATABASE IF NOT EXISTS health_recipe";
