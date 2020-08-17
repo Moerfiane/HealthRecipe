5b1f7826496fd0a8c9acc1908a5e7a4c3ed62540
Laa <lastembo@gmail.com>
2020-08-17 16:11:59 -0500

    Lisa28 - add.html changed th:field="${ingredient.ingredient}" to th:field"${ingredient.ingredientName}", change Ingredient model private field ingredient to private field ingredientName, added @OneToMany @ManyToOne annotation for foreign key relationship in recipe table.  actually changed ingredientName to recipeIngredients recommended by Kathy

--- src/main/java/org/launchcode/health_recipe/models/Ingredient.java
+++ src/main/java/org/launchcode/health_recipe/models/Ingredient.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @Entity(name = "Ingredient")
@@ -42,5 +43,19 @@ public class Ingredient extends AbstractEntity {
 
     public void setRecipes(List<Recipe> recipes) {
         this.recipes = recipes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ingredient)) return false;
+        if (!super.equals(o)) return false;
+        Ingredient that = (Ingredient) o;
+        return getRecipeIngredients().equals(that.getRecipeIngredients());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getRecipeIngredients());
     }
 }
