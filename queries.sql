ad3786b9a62e23f59a71602287c3465348d93571
Laa <lastembo@gmail.com>
2020-08-21 17:59:09 -0500

    LisaKathy41 - restored CSS to the correct code to fix recipe card display.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,35 @@
 ## Part 1: Test it with SQL
+   INSERT INTO dietary_restrictions_search (restrict_id, health_conds)
+	VALUES 	(101,"Celiac Disease"),
+			(102,"Diabetes"),
+			(103,"High Blood Pressure");
+
+	INSERT INTO dietary_restrictions_search (restrict_id, ingred_search)
+    VALUES   (201,"Pork"),
+            (202,"Chicken"),
+            (203,"Beef"),
+            (204,"Cucumbers"),
+            (205,"Cheese"),
+            (206,"Peppers"),
+            (207,"Fish"),
+            (208,"Onions"),
+            (209,"Nuts"),
+            (210,"Vegetables");
+
+  INSERT INTO dietary_restrictions_search (restrict_id, restrictions)
+	VALUES 	(301,"No Carbohydrates"),
+			(302,"No Dairy"),
+			(303,"No Saturated Fats"),
+			(304,"No Sodium"),
+			(305,"Gluten Free"),
+			(306,"Nitrate Free"),
+			(307,"Sodium Free");
+
+	INSERT INTO dietary_restrictions_search (restrict_id, cook_time)
+    VALUES   (401,"15 Minutes"),
+            (402,"30 Minutes"),
+            (403,"45 Minutes"),
+            (404,"60 Minutes");
 
 
 --## Part 2: Test it with SQL
